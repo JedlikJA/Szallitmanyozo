5662b8dcf18e9213687f7ae6339a8472e97ba014
juhász.ábel.kristóf <juhasz.abel99@gmail.com>
2020-02-21 09:11:40 +0100

    három üres függvény

--- szallitmanyozo/szallitmanyozo/Program.cs
+++ szallitmanyozo/szallitmanyozo/Program.cs
@@ -10,6 +10,26 @@ namespace szallitmanyozo
     {
         static void Main(string[] args)
         {
+            string[] teljes = { "ABC-123", "ASD-234", "FGH-673", "FCK-666", "SEX-069" };
+            List<string> bent = new List<string>();
+            string választás;
+            do
+            {
+                //A választási lehetőségek kiírása
+                Console.WriteLine("Elmegy (E)");
+                Console.WriteLine("Bejön (B)");
+                Console.WriteLine("Kilépés (K)");
+                //A válasz bekérése
+                választás = Console.ReadLine();
+                switch (választás)
+                {
+                    case "E": Elmegy(); break;
+                    case "B": Bejön(); break;
+                    case "K": Kilépés(); break;
+                    default: Console.WriteLine("Nem megyfelelő választás"); break;
+                }
+            } while (választás != "K");
+            Console.ReadKey();
         }
     }
 }
